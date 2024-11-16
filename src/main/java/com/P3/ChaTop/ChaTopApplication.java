c4470e1814a69b18f9af90d5980f19ac0b61b677
Shikizzz <widemann.simon@outlook.fr>
2024-11-16 14:28:13 +0100

    first commit

---
+++ src/main/java/com/P3/ChaTop/ChaTopApplication.java
@@ -0,0 +1,13 @@
+package com.P3.ChaTop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaTopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChaTopApplication.class, args);
+	}
+
+}
